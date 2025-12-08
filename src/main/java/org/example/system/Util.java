fc8d403612875b8a3c5c04cbeb35bfef2c2f691a
doun9711-ai <doun9711@gmail.com>
2025-12-08 17:20:08 +0900

    게시글 검색기능 추가중

---
+++ src/main/java/org/example/system/Util.java
@@ -0,0 +1,13 @@
+package org.example.system;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Util {
+    public static String getNowStr() {
+        LocalDateTime now = LocalDateTime.now();
+        String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        return formatedNow;
+    }
+
+}
---
+++ src/main/java/org/example/system/makeTestDatas.java
@@ -0,0 +1,162 @@
+package org.example.system;
+
+
+
+import java.util.ArrayList;
+
+
+
+class makeTestDatas {
+
+
+    void main() {
+        ArrayList<Article> articles1 = new ArrayList<>();
+        ArrayList<Article> articles2 = new ArrayList<>();
+        ArrayList<Article> articles3 = new ArrayList<>();
+        class articles1 {
+
+            private int id = 1;
+            private String regDate = "2025_12_07 13_26_30";
+            private String updateDate;
+            private String title = "tset1";
+            private String body = "test1";
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getRegDate() {
+                return regDate;
+            }
+
+            public void setRegDate(String regDate) {
+                this.regDate = regDate;
+            }
+
+            public String getUpdateDate() {
+                return updateDate;
+            }
+
+            public void setUpdateDate(String updateDate) {
+                this.updateDate = updateDate;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getBody() {
+                return body;
+            }
+
+            public void setBody(String body) {
+                this.body = body;
+            }
+        }
+        class articles2 {
+
+            private int id = 1;
+            private String regDate = "2025_12_08 13_26_30";
+            private String updateDate;
+            private String title = "tset2";
+            private String body = "test2";
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getRegDate() {
+                return regDate;
+            }
+
+            public void setRegDate(String regDate) {
+                this.regDate = regDate;
+            }
+
+            public String getUpdateDate() {
+                return updateDate;
+            }
+
+            public void setUpdateDate(String updateDate) {
+                this.updateDate = updateDate;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getBody() {
+                return body;
+            }
+
+            public void setBody(String body) {
+                this.body = body;
+            }
+        }
+        class articles3 {
+
+            private int id = 1;
+            private String regDate = "2025_12_08 14_30_30";
+            private String updateDate;
+            private String title = "tset3";
+            private String body = "test3";
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getRegDate() {
+                return regDate;
+            }
+
+            public void setRegDate(String regDate) {
+                this.regDate = regDate;
+            }
+
+            public String getUpdateDate() {
+                return updateDate;
+            }
+
+            public void setUpdateDate(String updateDate) {
+                this.updateDate = updateDate;
+            }
+
+            public String getTitle() {
+                return title;
+            }
+
+            public void setTitle(String title) {
+                this.title = title;
+            }
+
+            public String getBody() {
+                return body;
+            }
+
+            public void setBody(String body) {
+                this.body = body;
+            }
+        }
+
+    }
+}
